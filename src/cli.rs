bbd5f0efe7cf9f9af1bc3e9c718e6b80ee9424f8
pascaldachard <Pasdachard@gmail.com>
2025-03-07 17:45:50 +0100

    feat: garlic init implemented

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,9 @@
 use std::{
-    io,
-    process::{exit, Command, ExitStatus},
+    ffi::OsStr,
+    fmt::Display,
+    fs,
+    path::{Path, PathBuf},
+    process::{exit, Command},
 };
 
 use clap::command;
@@ -11,12 +14,14 @@ use colored::Colorize;
 #[command(author, version, about, long_about = None)]
 pub struct Cli {
     #[clap(subcommand)]
-    pub command: Cmd,
+    pub command: GarlicCommand,
 }
 
 #[derive(clap::Subcommand)]
-pub enum Cmd {
-    Init,
+pub enum GarlicCommand {
+    Init {
+        location: Option<String>,
+    },
     /// Runs the backend. aliases = (backend, server)
     #[command(name = "run:backend", aliases=["backend", "server"])]
     RunBackend,
@@ -37,31 +42,131 @@ pub enum Cmd {
     Migrate,
 }
 
-pub fn command(command: &str) {
-    let mut args = command.split(" ");
-    let cmd = args.next().expect("Expected command to not be empty");
+#[must_use]
+pub struct Cmd(Command, String);
+impl Cmd {
+    pub fn run(command: &str) -> Self {
+        let mut args = command.split(" ");
+        let cmd = args.next().expect("Expected command to not be empty");
+        let mut cmd = Command::new(cmd);
+        cmd.args(args);
 
-    println!("{}: Running \"{}\"", "[garlic]".green(), command.cyan());
+        Self(cmd, command.to_owned())
+    }
 
-    let mut cmd = Command::new(cmd);
-    cmd.args(args);
+    pub fn arg(mut self, arg: impl Display + AsRef<OsStr>) -> Self {
+        self.1.extend(format!(" {arg}").chars());
+        self.0.arg(arg);
+        self
+    }
 
-    print_status(cmd.status());
+    fn display(&self) {
+        println!("{}: Running \"{}\"", "[garlic]".green(), self.1.cyan());
+    }
+
+    pub fn req(mut self) {
+        self.display();
+        match self.0.status() {
+            Err(e) => error(e.kind(), e),
+            Ok(o) if !o.success() => exit(o.code().unwrap_or(1)),
+            _ => {}
+        }
+    }
+
+    pub fn opt(mut self) {
+        self.display();
+        match self.0.status() {
+            Err(e) => error_opt(e.kind(), e),
+            _ => {}
+        }
+    }
+
+    pub fn ok(mut self) -> bool {
+        self.display();
+        self.0.status().is_ok_and(|o| o.success())
+    }
 }
 
-fn print_status(status: io::Result<ExitStatus>) {
-    match status {
-        Err(e) => {
-            println!(
-                "{} (type {}): {}",
-                "Error".red(),
-                e.kind().to_string().cyan(),
-                e.to_string().red()
-            );
+pub fn garlic_print(content: impl Display) {
+    println!("{}: {}", "[garlic]".green(), content);
+}
 
-            exit(1)
-        }
-        Ok(o) if !o.success() => exit(o.code().unwrap_or(1)),
-        _ => {}
+pub fn error(kind: impl Display, message: impl Display) {
+    println!(
+        "{} (type {}): {}",
+        "Error".red(),
+        kind.to_string().cyan(),
+        message.to_string().red()
+    );
+
+    exit(1)
+}
+
+pub fn error_opt(kind: impl Display, message: impl Display) {
+    println!(
+        "{} (type {}): {}",
+        "Error".red(),
+        kind.to_string().cyan(),
+        message.to_string().red()
+    );
+}
+
+pub fn folder_empty<S: AsRef<Path>>(location: S) -> bool {
+    if !location.as_ref().is_dir() {
+        return false;
     }
+
+    let entries = fs::read_dir(location).expect("Expected a folder");
+    return entries.count() == 0;
+}
+
+pub fn copy_dir_contents(
+    from: impl AsRef<Path>,
+    to: impl AsRef<Path>,
+) -> Result<(), std::io::Error> {
+    let mut stack = Vec::new();
+    let from = from.as_ref();
+    stack.push(PathBuf::from(from));
+
+    let output_root = PathBuf::from(to.as_ref());
+    let input_root = PathBuf::from(from).components().count();
+
+    while let Some(working_path) = stack.pop() {
+        println!("process: {:?}", &working_path.strip_prefix(from));
+
+        // Generate a relative path
+        let src: PathBuf = working_path.components().skip(input_root).collect();
+
+        // Create a destination if missing
+        let dest = if src.components().count() == 0 {
+            output_root.clone()
+        } else {
+            output_root.join(&src)
+        };
+        if fs::metadata(&dest).is_err() {
+            println!(" mkdir: {:?}", dest);
+            fs::create_dir_all(&dest)?;
+        }
+
+        for entry in fs::read_dir(working_path)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                stack.push(path);
+            } else {
+                match path.file_name() {
+                    Some(filename) => {
+                        let dest_path = dest.join(filename);
+                        println!("  copying {:?}", &path.strip_prefix(from),);
+                        fs::copy(&path, &dest_path)?;
+                    }
+                    None => {
+                        println!("failed: {:?}", path);
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,89 @@ mod cli;
 
 use clap::Parser;
 pub use cli::*;
+use dialoguer::Confirm;
+use tempdir::TempDir;
+pub use GarlicCommand as Cc;
+
+const SPEC_GET: &str = "bun openapi-zod-client spec.yml -o ./src/lib/gen/client.ts";
+const SQLX_PREPARE: &str = "cargo sqlx prepare --workspace";
 
 fn main() {
     match Cli::parse().command {
-        Cmd::Init => command("cargo install cargo-edit"),
-        Cmd::RunBackend => command("cargo run"),
-        Cmd::RunFrontend => command("bun x vite dev"),
-        Cmd::Build => {
-            command("bun x vite build");
-            command("cargo build --release")
-        }
-        Cmd::Prepare => command("cargo sqlx prepare --workspace"),
-        Cmd::Preview => command("bun x vite preview"),
-        Cmd::TestUnit => command("bun x vitest"),
-        Cmd::TestAll => {
-            command("cargo test");
-            command(SPEC_GET);
-            command("bun x vitest --run")
-        }
-        Cmd::Spec => {
-            command("cargo set-version --bump patch --package server");
+        Cc::Init { location } => {
+            let location = location.as_deref().unwrap_or(".");
+            init_inner();
+            if !folder_empty(location)
+                && Confirm::new()
+                    .with_prompt("Folder is not empty. Continue?")
+                    .interact()
+                    .unwrap_or(false)
+            {
+                return garlic_print("Folder is not empty. Exiting");
+            }
+            let tempdir = TempDir::new("garlic-init")
+                .expect("Expected to be able to create temporary directory");
 
-            command("cargo test test_load_spec");
-            command(SPEC_GET);
-            command("vitest spec --run")
+            let temp_str = tempdir.path().to_str().expect("expected utf-8 path");
+
+            Cmd::run("git clone https://github.com/eatenpancreas/garlic.git")
+                .arg(temp_str)
+                .req();
+
+            copy_dir_contents(temp_str, location).expect("Expected to be able to clone directory");
+
+            Cmd::run("git init").req();
         }
-        Cmd::Migrate => command("cargo sqlx migrate"),
+        Cc::RunBackend => Cmd::run("cargo run").req(),
+        Cc::RunFrontend => Cmd::run("bun x vite dev").req(),
+        Cc::Build => {
+            Cmd::run("bun x vite build").req();
+            Cmd::run("cargo build --release").req()
+        }
+        Cc::Prepare => Cmd::run(SQLX_PREPARE).req(),
+        Cc::Preview => Cmd::run("bun x vite preview").req(),
+        Cc::TestUnit => Cmd::run("bun x vitest").req(),
+        Cc::TestAll => {
+            Cmd::run("cargo test").req();
+            Cmd::run(SPEC_GET).req();
+            Cmd::run("bun x vitest --run").req()
+        }
+        Cc::Spec => {
+            Cmd::run("cargo set-version --bump patch --package server").req();
+
+            Cmd::run("cargo test test_load_spec").req();
+            Cmd::run(SPEC_GET).req();
+            Cmd::run("vitest spec --run").req()
+        }
+        Cc::Migrate => Cmd::run("cargo sqlx migrate").req(),
     }
 }
 
-const SPEC_GET: &str = "bun openapi-zod-client spec.yml -o ./src/lib/gen/client.ts";
+fn init_inner() {
+    if !Cmd::run("bun --version").ok() {
+        error(
+            "missing_install",
+            "Bun is not installed or is not in your environment",
+        );
+    }
+    if !Cmd::run("git --version").ok() {
+        error(
+            "missing_install",
+            "Git is not installed or is not in your environment",
+        );
+    }
+    if !Cmd::run("cargo --version").ok() {
+        error(
+            "missing_install",
+            "Cargo is not installed or is not in your environment",
+        );
+    }
+
+    if !Cmd::run("cargo set-version --version").ok() {
+        Cmd::run("cargo install cargo-edit").opt();
+    }
+
+    if !Cmd::run("cargo sqlx --version").ok() {
+        Cmd::run("cargo install sqlx-cli --features postgres").opt();
+    }
+}
